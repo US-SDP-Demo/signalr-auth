0edecac3e6d8a8bd4e94d182a31c50a909a00299
Mark Gardner <mark.gardner@microsoft.com>
2023-11-17 18:18:41 +0000

    adding chatroom example

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR().AddAzureSignalR();
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapHub<ChatSampleHub>("/chat");
+
+app.Run();
